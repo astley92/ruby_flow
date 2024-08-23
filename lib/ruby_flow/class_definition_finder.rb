1cb1048b0619f958721dc53cf0ae481de507df76
Blake Astley <astley92@hotmail.com>
2024-08-23 14:01:38 +1000

    Get main test cases passing

--- lib/ruby_flow/class_definition_finder.rb
+++ lib/ruby_flow/class_definition_finder.rb
@@ -6,25 +6,33 @@ require("byebug")
 module RubyFlow
   module ClassDefinitionFinder
     def self.call(content) # rubocop:disable Metrics/MethodLength
-      class_list = []
+      class_list = Set.new
       parsed_content = Parser::CurrentRuby.parse(content)
-      stack = [parsed_content]
+      stack = [[parsed_content, nil]]
       while stack.any?
-        current = stack.pop
+        current, path = stack.pop
         next if current.class != Parser::AST::Node
 
-        if current.type == :class
+        if current.type == :class || current.type == :module
           const_child = current.children.first
           class_name = const_child.loc.expression.source
-          class_list << class_name
+          parentclasses = class_name.split("::")[...-1]
+          while parentclasses.any?
+            class_list << parentclasses.join("::")
+            parentclasses = parentclasses[...-1]
+          end
+          class_list << [path, class_name].compact.join("::")
+          path = [path, class_name].compact.join("::")
         end
 
-        stack += current.children
+        current.children.each do |child|
+          stack << [child, path]
+        end
       end
 
-      class_list
+      class_list.to_a
     rescue Parser::SyntaxError
-      class_list
+      class_list.to_a
     end
   end
 end
