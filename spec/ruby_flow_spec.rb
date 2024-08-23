e236aea40885f2ecabd6ff56fffe39af6c48af41
Blake Astley <astley92@hotmail.com>
2024-08-23 13:09:07 +1000

    Initial commit

---
+++ spec/ruby_flow_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyFlow do
+  it "has a version number" do
+    expect(RubyFlow::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
